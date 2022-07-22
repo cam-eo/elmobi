afad8e2b6b41c055d6d45b0f191332ed90b40235
cam <cameron.n.wright@gmail.com>
2022-07-22 18:09:08 +0200

    some mapping, conditional formatting and on click handling

--- src/App.svelte
+++ src/App.svelte
@@ -1,45 +1,84 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import wheel from "./assets/icons/wheel.svg";
+  const menuItems = [
+    { name: "one", id: 1 },
+    { name: "two", id: 2 },
+    { name: "three", id: 3 },
+    { name: "four", id: 4 },
+  ];
+
+  let headerState = menuItems[0];
+  console.log("headerState: ", headerState);
 </script>
 
 <main>
+  <div class="header">
+    {#each [...menuItems.values()] as value}
+      <div
+        class="headerItem"
+        on:click={() => {
+          headerState = value;
+        }}
+        style="--opacity: {`${value.id === headerState.id ? 1 : 0.5}`};"
+      >
+        <img src={wheel} class="wheel" alt="Wheel" />
+        {#if value.id !== 1}
+          {#each Array(value.id - 1) as _, index (index)}
+            <img
+              src={wheel}
+              class="wheel_subtle"
+              alt="Wheel"
+              style="--transform: {`${(index + 1) * 15}px`}; --zIndex: {-(
+                index + 1
+              )}"
+            />
+          {/each}
+        {/if}
+        <span class="headerText">{value.name}</span>
+      </div>
+    {/each}
+  </div>
   <div>
-    <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <a href="https://svelte.dev" target="_blank">HI</a>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
+  /* .test {
+    z-index: 100;
+  } */
+  .wheel {
+    height: 100px;
+    border-radius: 50px;
+    background-color: brown;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+  .wheel_subtle {
+    height: 100px;
+    background-color: grey;
+    position: absolute;
+    z-index: var(--zIndex);
+    transform: translateX(var(--transform));
+    border-radius: 50px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  .header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 700px;
   }
-  .read-the-docs {
-    color: #888;
+
+  .headerItem {
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    cursor: pointer;
+    opacity: var(--opacity);
+    transition: opacity 0.5s;
+  }
+
+  .headerText {
+    position: absolute;
+    z-index: 1;
   }
 </style>
