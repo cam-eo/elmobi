e95ffaace35651bbd3287b9dec0fcd7b04c61125
cam <cameron.n.wright@gmail.com>
2022-07-23 16:33:47 +0200

    starting to build up a searchbox of some kind

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,38 @@
 <script>
   import Header from "./lib/Header.svelte";
   import CarListItem from "./lib/CarListItem.svelte";
-  import teslaMX from "./assets/images/tesla_modelx.jpg";
-  import teslaLogo from "./assets/logos/tesla_logo.svg";
+
+  const searchbox = {
+    type: ["car", "pickup"],
+    brand: ["m"],
+  };
 </script>
 
 <main>
   <Header />
+  <form class="searchbox">
+    <div class="dropdown">
+      <select name="cars" id="cars">
+        <option hidden>Type...</option>
+        <option value="volvo">Volvo</option>
+        <option value="saab">Saab</option>
+        <option value="mercedes">Mercedes</option>
+        <option value="audi">Audi</option>
+      </select>
+    </div>
+  </form>
   <CarListItem />
 </main>
 
 <style>
+  .dropdown {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+  }
+
+  .dropdown > select {
+    width: 130px;
+    height: 30px;
+  }
 </style>
